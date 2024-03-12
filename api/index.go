2f59375ed2233412d6b928e7168fd8aa73261003
todoshi <ttodoshi@users.noreply.github.com>
2024-03-12 23:00:11 +0300

    fix: fixed template

--- api/index.go
+++ api/index.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GenerateDemotivator(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
+	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
